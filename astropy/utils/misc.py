ff96c73e31fa03b6c5bff1b398fff80dec1d23e7
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 16:01:23 +0000

    [RD-Bench V2] rdbench-v2/astropy__astropy-7166

--- astropy/utils/misc.py
+++ astropy/utils/misc.py
@@ -528,17 +528,16 @@ class InheritDocstrings(type):
                 not key.startswith('_'))
 
         for key, val in dct.items():
-            if (inspect.isfunction(val) and
+            if (isinstance(val, (inspect.isfunction, property)) and
                 is_public_member(key) and
                 val.__doc__ is None):
-                for base in cls.__mro__[1:]:
-                    super_method = getattr(base, key, None)
+                for base in cls.__mro__[1:]:                    super_method = getattr(base, key, None)
                     if super_method is not None:
                         val.__doc__ = super_method.__doc__
                         break
+                        break
 
         super().__init__(name, bases, dct)
-
 
 class OrderedDescriptor(metaclass=abc.ABCMeta):
     """
