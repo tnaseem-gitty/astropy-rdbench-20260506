b2bb96026c0b2daf9be2e096495a70173ab60831
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 15:20:15 +0000

    [RD-Bench V2] rdbench-v2/astropy__astropy-14365

--- astropy/io/ascii/qdp.py
+++ astropy/io/ascii/qdp.py
@@ -60,7 +60,7 @@ def _line_type(line, delimiter=None):
     ValueError: Unrecognized QDP line...
     """
     _decimal_re = r"[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?"
-    _command_re = r"READ [TS]ERR(\s+[0-9]+)+"
+    _command_re = r"(?i)READ [TS]ERR(\s+[0-9]+)+"
 
     sep = delimiter
     if delimiter is None:
