e3622145cece7e5cf1549688cc2535a5cc261712
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 15:20:23 +0000

    [RD-Bench V2] rdbench-v2/astropy__astropy-14995

--- astropy/nddata/mixins/ndarithmetic.py
+++ astropy/nddata/mixins/ndarithmetic.py
@@ -520,7 +520,7 @@ class NDArithmeticMixin:
         elif self.mask is None and operand is not None:
             # Make a copy so there is no reference in the result.
             return deepcopy(operand.mask)
-        elif operand is None:
+        elif operand is None or operand.mask is None:
             return deepcopy(self.mask)
         else:
             # Now lets calculate the resulting mask (operation enforces copy)
